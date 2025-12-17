b3b305ed04941667241541956ef1c4ed70a0faed
mahdiJD <bijarimahdi@gmail.com>
2025-12-17 18:20:40 +0330

    first practise

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 // console.log("Hello JS Backend");
-function add(a,b){
-    return a+b;
+
+// function add(a,b){
+//     return a+b;
+// }
+// const result = add(1,2);
+// console.log(result);
+
+function isEven(num){
+    return num%2===0;
 }
-const result = add(1,2);
-console.log(result);
+function isOdd(num){
+    return num%2===1;
+}
+
+console.log(isEven(2));
+console.log(isOdd(2));
+
+// practise 1
+function typeValidation(arg,type){
+    if( typeof arg !== type ){
+        console.log(`error : ${arg} must be a ${type}.`);
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function userInfo(name,age){
+    const validArg = typeValidation(name,"string") && typeValidation(age,"number");
+    if(!validArg){
+        return null
+    }
+    return `My name is ${name} and I am ${age} years old`;
+}
+console.log(userInfo("Mahdi",24))
